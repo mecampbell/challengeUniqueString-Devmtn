904fe0f81dd01cf0e02853f6e731c9380154ec8b
Michael Campbell <michaelmichael323@hotmail.com>
2022-05-21 15:05:57 -0600

    initial commit

---
+++ makeUnique.js
@@ -0,0 +1,12 @@
+// Write a function to remove all duplciate letters from a provided string. 
+// The string will only contain lowercase letters between a - z. 
+// The string will not contain spaces.
+
+// For example:
+
+// >>> makeUnique('helloworld')
+// // helowrd
+
+// >>> makeUnique('iwanttoclimbamountain')
+// // iwantoclmbu
+
